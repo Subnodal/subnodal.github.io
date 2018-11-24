e20f0ff4a1f6412c49cc561bbc38c27f0473d0d5
James-Livesey <jamesliveg@gmail.com>
2018-11-24 11:08:01 +0000

    Add project info and header

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 function scrollDown() {
     $([document.documentElement, document.body]).animate({
-        scrollTop: $("section").not(".cover")[0].offsetTop
+        scrollTop: $("section").not(".cover")[0].offsetTop - 80
     }, 1000);
 }
 
 $(function() {
+    if ($(window).scrollTop() >= 20) {
+        $(".arrowContainer").hide();
+    }
+
+    $("header").hide();
+
     $(window).scroll(function() {
         if ($(this).scrollTop() >= 20) {
             $(".arrowContainer").fadeOut();
         } else {
             $(".arrowContainer").fadeIn();
+        }
+
+        if ($(this).scrollTop() >= ($("section.cover").height() / 1.5) - 50) {
+            $("header").fadeIn();
+        } else {
+            $("header").fadeOut();
         }
     });
   });
