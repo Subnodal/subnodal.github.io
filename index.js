70f6512686eda0cce95b0d41a40d58d92f8265a3
Sebastian Doe <39373619+sebastiandoe5@users.noreply.github.com>
2019-06-07 13:32:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function() {
-    var coverText = _("We're {\1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaamazon\2}");
+    var coverText = _("We're {\1subnodal\2}");
     var doStrong = false;
 
     $("#coverAnimation").html("");
