c48f020d97b21b8ea3a7b4836edcf87a96eaab3b
Sebastian Doe <39373619+sebastiandoe5@users.noreply.github.com>
2022-04-29 13:39:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function() {
-    var coverText = _("We're {\1subway\2}");
+    var coverText = _("We're {\1subnooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooodal\2}");
     var doStrong = false;
 
     $("#coverAnimation").html("");
